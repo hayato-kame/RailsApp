0ccd46b24aef79ea00906ac68e62259abf1baaac
hayato-kao <rsz42916@nifty.com>
2021-05-09 16:18:20 +0900

    create people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,8 +3,47 @@ class PeopleController < ApplicationController
   def index
     @msg = 'Person data'
     @data = Person.all
+    @f_msg = params[:f_msg]
     pp @data
     # binding.pry
     render 'index'
   end
+
+  def show
+    @mdg = '検索結果'
+    @data = Person.find(params[:id])
+    pp @data
+    # binding.pry
+    render 'show'
+  end
+
+  def add
+    @msg = 'add new data.'
+    @person = Person.new
+    render 'add'
+  end
+
+  # def create
+  #   if request.post? then
+  #     obj = Person.create([
+  #       {name: params['name'], age: params['age'], mail: params['mail'] }
+  #     ])
+  #   end
+  #   redirect_to '/people?f_msg=成功'
+  #   # redirect_to controller: 'people', action: 'index', params: {f_msg: '作成成功しました'}
+  # end
+
+  def create
+    if request.post? then
+      Person.create(person_params)
+    end
+    # redirect_to '/people?f_msg=登録できたよ〜〜〜〜〜'
+    redirect_to controller: 'people', action: 'index', params: {f_msg: '登録成功！！！'}
+  end
+
+  private
+  def person_params
+    params.require(:person).permit(:name, :age, :mail)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'people/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'people/index'
   get 'people', to: 'people#index'
 
+  get 'people/add'
+  post 'people/add', to: 'people#create'
+
+  # パラメータ付きアドレスのルーティングは関係するルーティングの一番下に書いてください
+  get 'people/:id', to: 'people#show'
 end
